a5ecca78005b7859d3db025abe3b46c6f7d2fe04
djnautiyal <dhannanjainautiyal@gmail.com>
2024-06-23 19:41:41 +0530

    adapter: weight from kg to pounds

--- src/com/learn/design_patterns/structural/adapter/weight_from_kg_to_pounds/WeightMachine.java
+++ src/com/learn/design_patterns/structural/adapter/weight_from_kg_to_pounds/WeightMachine.java
@@ -1,9 +1,5 @@
 package com.learn.design_patterns.structural.adapter.weight_from_kg_to_pounds;
 
-import com.learn.design_patterns.behavioural.interpreter.binary_expression_interpreter.Interpreter;
-
-import java.util.function.Supplier;
-
 public class WeightMachine {
     int weight = 93;
 
